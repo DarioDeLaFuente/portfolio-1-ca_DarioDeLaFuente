d35d3dbe26079977392acb07be86427c4d84b4e6
DarioDeLaFuente <livet.til.dario@gmail.com>
2022-10-07 19:46:33 +0200

    adding custum css to bootstrap

--- src/js/components/posts.js
+++ src/js/components/posts.js
@@ -43,26 +43,16 @@ function postsListDetails(posts) {
                 <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1 layer">
                   <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">${posts[i].title.rendered}</h3>
                   <ul class="d-flex list-unstyled mt-auto">
+                  <li class="d-flex align-items-center">
+                      <small>${posts[i].excerpt.rendered}</small>
+                    </li>
+                  </ul>
+                  <ul class="d-flex list-unstyled mt-auto">
                     <li class="me-auto">
-                      <img
-                        src="https://github.com/twbs.png"
-                        alt="Bootstrap"
-                        width="32"
-                        height="32"
-                        class="rounded-circle border border-white"
-                      />
-                    </li>
-                    <li class="d-flex align-items-center me-3">
-                      <svg class="bi me-2" width="1em" height="1em">
-                        <use xlink:href="/post-project-results.html?post_id=${posts[i].id}"></use>
-                      </svg>
-                      <small>${posts[i].title.rendered}</small>
-                    </li>
-                    <li class="d-flex align-items-center">
-                      <svg class="bi me-2" width="1em" height="1em">
-                        <use xlink:href="#calendar3"></use>
-                      </svg>
-                      <small>${posts[i].type} ${posts[i].status}:${formattedDate}</small>
+                      <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-index-thumb-fill" viewBox="0 0 16 16">
+                      <path d="M8.5 1.75v2.716l.047-.002c.312-.012.742-.016 1.051.046.28.056.543.18.738.288.273.152.456.385.56.642l.132-.012c.312-.024.794-.038 1.158.108.37.148.689.487.88.716.075.09.141.175.195.248h.582a2 2 0 0 1 1.99 2.199l-.272 2.715a3.5 3.5 0 0 1-.444 1.389l-1.395 2.441A1.5 1.5 0 0 1 12.42 16H6.118a1.5 1.5 0 0 1-1.342-.83l-1.215-2.43L1.07 8.589a1.517 1.517 0 0 1 2.373-1.852L5 8.293V1.75a1.75 1.75 0 0 1 3.5 0z"/>
+                    </svg>
+                    <small>${posts[i].status}:${formattedDate}</small>
                     </li>
                   </ul>
                 </div>
